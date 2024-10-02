68c411861f314dc391bf9d9b8a193c3be7b6d843
efhunt <63171168+efhunt@users.noreply.github.com>
2024-10-02 13:17:07 +0100

    setting up repository and adding first tool

--- README.rst
+++ README.rst
@@ -1,57 +1,10 @@
 
-PeleAnalysis
+PeleAnalysisAdditions
 ============
 
-This repository contains a collection of standalone routines for processing plotfiles created with the AMReX software framework for block-structured adaptive mesh refinement simulations.  Documentation is under development, but is
-available at ::
-
-   https://peleanalysis.readthedocs.io/en/latest/
-
-AMReX is required for these tools, and is available https://github.com/AMReX-Codes/amrex
-
-In order to build these processing tools, you should clone or fork the amrex repository, and set the environment variable ``AMREX_HOME`` to point to the local folder where that is placed.  Then clone this repository, ``cd Src`` and edit the ``GNUmakefile`` to select which tool to build.  If AMReX is configured properly, a stand-alone executable will be built locally, based on the selected options, including spatial dimension (2 or 3), compiler choices, whether to build with MPI and/or OpenMP enabled, and whether to build a debugging or optimized version.  Note that some of the tools require building a companion f90 source file - you must manually set the flag in the ``GNUmakefile`` accordingly.  More extensive documentation is available (see building instructions below).
-
-Contributions
--------------
-
-To add a new feature to PeleAnalysis, the procedure is:
-
-1. Create a branch for the new feature (locally) ::
-
-    git checkout -b AmazingNewFeature
-
-2. Develop the feature, merging changes often from the `master` branch into your AmazingNewFeature branch ::
-   
-    git commit -m "Developed AmazingNewFeature"
-    git checkout master
-    git pull                     [fix any identified conflicts between local and remote branches of "master"]
-    git checkout AmazingNewFeature
-    git merge master        [fix any identified conflicts between "master" and "AmazingNewFeature"]
-
-3. Push feature branch to PeleAnalysis repository (if you have write access, otherwise fork the repo and
-push the new branch to your fork)::
-
-    git push -u origin AmazingNewFeature [Note: -u option required only for the first push of new branch]
-
-4.  Submit a merge request through the github project page - be sure you are requesting to merge your branch to the master branch.
-
-
-
-
-Documentation
--------------
-Documentation for the analysis routines exists in the Docs directory. To build the documentation::
-
-    cd Docs
-    make html
+This repository contains an additional collection of tools yet (or not appropriate) to be pushed to PeleAnalysis.
 
 
 Acknowledgment
 --------------
-This research was supported by the Exascale Computing Project (ECP), Project
-Number: 17-SC-20-SC, a collaborative effort of two DOE organizations -- the
-Office of Science and the National Nuclear Security Administration --
-responsible for the planning and preparation of a capable exascale ecosystem --
-including software, applications, hardware, advanced system engineering, and
-early testbed platforms -- to support the nation's exascale computing
-imperative.
+PeleAnalysis was supported by the Exascale Computing Project (ECP), Project Number: 17-SC-20-SC, a collaborative effort of two DOE organizations -- the Office of Science and the National Nuclear Security Administration -- responsible for the planning and preparation of a capable exascale ecosystem -- including software, applications, hardware, advanced system engineering, and early testbed platforms -- to support the nation's exascale computing imperative.
--- Src/plotProg.cpp
+++ Src/plotProg.cpp
@@ -15,7 +15,7 @@ print_usage (int,
              char* argv[])
 {
   std::cerr << "usage:\n";
-  std::cerr << argv[0] << " infile infile=f1 [options] \n\tOptions:\n";
+  std::cerr << argv[0] << " infile=plt????? vars= ?? [options] \n";
   exit(1);
 }
 
@@ -47,10 +47,42 @@ main (int   argc,
     DataServices::SetBatchMode();
     Amrvis::FileType fileType(Amrvis::NEWPLT);
 
+    // pass outfile name
+    std::string outfile(getFileRoot(plotFileName) + "_prog");
+    pp.query("outfile",outfile);
+
+    // get progress variable names
+    int nComp(pp.countval("vars"));
+    if (nComp <= 0)  Abort("No progress varbiable names given");
+    Vector<std::string> inVarNames;
+    inVarNames.resize(nComp);
+    pp.getarr("vars",inVarNames);
+    
+    // normalise by specific plotfile
+    std::string normalisePltName; pp.query("normalisePltName",normalisePltName);
+    bool normalisePlt=1;
+    if (normalisePltName.empty())
+	normalisePlt=0;
+    
+    Print() << "progress variable names: ";
+    for (int comp = 0; comp < nComp; comp++)
+	Print() << inVarNames[comp] << " ";
+    Print() << "\n";
+
+    // output data names
+    Vector<std::string> outVarNames = inVarNames;
+    
+    // data used to normalise
+    DataServices normaliseDataServices(normalisePltName, fileType);
+    if( ! normaliseDataServices.AmrDataOk() && 1 == normalisePlt) {
+	DataServices::Dispatch(DataServices::ExitRequest, NULL);
+    }
+    AmrData& normaliseAmrData = normaliseDataServices.AmrDataRef(); // this does send a runtime warning error if not normalising
+
+    // data to process
     DataServices dataServices(plotFileName, fileType);
     if( ! dataServices.AmrDataOk()) {
-      DataServices::Dispatch(DataServices::ExitRequest, NULL);
-      // ^^^ this calls ParallelDescriptor::EndParallel() and exit()
+	DataServices::Dispatch(DataServices::ExitRequest, NULL);
     }
     AmrData& amrData = dataServices.AmrDataRef();
 
@@ -70,17 +102,26 @@ main (int   argc,
     Vector<MultiFab> outdata(Nlev);
     Vector<Geometry> geoms(Nlev);
 
-    int nGrow = 0;
-    int nComp = amrData.NComp();
+    const int nGrow = 0;
 
-    Vector<std::string> inNames = amrData.PlotVarNames();
-    Vector<std::string> outNames = amrData.PlotVarNames();
-    
     Vector<int> destFillComps(nComp);
     for (int i=0; i<nComp; ++i) destFillComps[i] = i;
 
-    for (int lev = 0; lev < Nlev; ++lev) {
-      
+    // find min/max to be normalised
+    Vector<Real> prog_burnt(nComp);
+    Vector<Real> prog_unburnt(nComp);
+
+    Print() << "nComp = " << nComp << std::endl;
+    for (int comp=0; comp < nComp; comp++) {
+	if (normalisePlt) {
+	    normaliseAmrData.MinMax(normaliseAmrData.ProbDomain()[0],inVarNames[comp],0,prog_burnt[comp], prog_unburnt[comp]);    
+	}
+	else {
+	    amrData.MinMax(amrData.ProbDomain()[0],inVarNames[comp],0,prog_burnt[comp], prog_unburnt[comp]);
+	}
+    }
+   
+    for (int lev = 0; lev < Nlev; ++lev) {      
       const BoxArray ba = amrData.boxArray(lev);
       const DistributionMapping dm(ba);
 
@@ -88,10 +129,9 @@ main (int   argc,
       MultiFab indata(ba,dm,nComp,nGrow);
 
       int coord = 0;
-      geoms[lev] = Geometry(amrData.ProbDomain()[lev],&rb, coord, &(is_per[0]));      
-      
+      geoms[lev] = Geometry(amrData.ProbDomain()[lev],&rb, coord, &(is_per[0]));            
       Print() << "Reading data for level " << lev << std::endl;
-      amrData.FillVar(indata,lev,inNames,destFillComps);
+      amrData.FillVar(indata,lev,inVarNames,destFillComps);
       Print() << "Data has been read for level " << lev << std::endl;
       
 #ifdef AMREX_USE_OMP
@@ -104,23 +144,19 @@ main (int   argc,
 	  auto const& in_a = indata.array(mfi);
 	  amrex::ParallelFor(bx, [=]
 			     AMREX_GPU_DEVICE (int i, int j, int k) noexcept
-				 {
-				   // Do something funky
-				   for (int n = 0; n < nComp; n++) {
-				     out_a(i,j,k,n) = in_a(i,j,k,n); 
-				   }
-				 });
+	      {
+		  for (int n = 0; n < nComp; n++) {
+		      out_a(i,j,k,n) = (in_a(i,j,k,n) - prog_unburnt[n]) / (prog_burnt[n] - prog_unburnt[n]);
+		  }
+	      });
 	}
-
     }
     
-    std::string outfile(getFileRoot(plotFileName) + "_temp");
     Print() << "Writing new data to " << outfile << std::endl;
     Vector<int> isteps(Nlev, 0);
     Vector<IntVect> refRatios(Nlev-1,{AMREX_D_DECL(2, 2, 2)});
-    amrex::WriteMultiLevelPlotfile(outfile, Nlev, GetVecOfConstPtrs(outdata), outNames,
+    amrex::WriteMultiLevelPlotfile(outfile, Nlev, GetVecOfConstPtrs(outdata), outVarNames,
                                    geoms, 0.0, isteps, refRatios);
-
   }
   Finalize();
   return 0;
